82a9929a555b72ba2cd26cca300727476b6b5086
Nuria-Oliveira <noliveira@alumno.unlam.edu.ar>
2026-04-26 12:27:44 -0300

    Estructura inicial proyecto

---
+++ main.c
@@ -0,0 +1,27 @@
+/*
+Apellido: Lopez Nina, Aylin Yussel
+DNI: 94593626
+Usuario: AylinLopez05
+Entrega: Sí
+
+Apellido: Magnone, Mauro Marcelo
+DNI: 42565840
+Usuario: Mauro-Mag
+Entrega: Sí
+
+Apellido: Oliveira, Nuria Elisa
+DNI: 31652404
+Usuario: Nuria-Oliveira
+Entrega: Sí
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
